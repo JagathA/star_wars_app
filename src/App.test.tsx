21c8a430d61632d071c06da03a7046272379f040
JagathA <jagath.ariy@gmail.com>
2023-02-24 19:07:20 +0000

    Added tests. One test fails

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 
-test('Check title is rendered correctley', () => {
+const charName = "Luke Skywalker";
+
+const server = setupServer(
+  rest.get('https://swapi.dev/api/people/1', (req, res, ctx) => {
+    return res(ctx.json({
+      "name": "Luke Skywalker"
+    }))
+  }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test('Check title is rendered correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Starwars Character/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Starwars Character/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+
+test('Check character data is rendered correctly', async () => {
+  render(<App />);
+
+  await screen.findByText(/Starwars Character/i);
+  await screen.findByText(/Name :/i);
+  const textElement = screen.getByText(/Luke Skywalker/i);
+  expect(textElement).toBeInTheDocument();
 });
