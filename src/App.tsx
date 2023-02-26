0cc10db31213fde800749262ef660e82404587ae
JagathA <jagath.ariy@gmail.com>
2023-02-26 17:18:45 +0000

    added API error handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { setDefaultResultOrder } from 'dns';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  interface StarWarsCharacter{
+  interface StarWarsCharacter {
     name: string;
   }
 
   const [character, setCharacter] = useState<string>("");
+  const [errorMsg, setErrorMsg] = useState<string | undefined>("");
 
   useEffect(() => {
     getCharacter(1);
@@ -14,19 +16,34 @@ const App: React.FC = () => {
 
   const getCharacter = async (id: number) => {
     const apiResponse = await fetch(`https://swapi.dev/api/people/${id}`);
-    const json = await apiResponse.json();
-    setCharacter(json.name);
-    console.log("*********** =>", json);
-    console.log("****** =>",json.name);
-  };
+    try {
+      console.log("******=>***", apiResponse);
+      if (apiResponse.ok) {
+        const json = await apiResponse.json();
+        console.log("response =>", apiResponse.status);
+        console.log("jason =>", json);
+        setCharacter(json.name);
+      }
+      else {
+        if (apiResponse.status === 500) {
+          setErrorMsg(`Oops... something went wrong, try again`);
+        }
+        else if (apiResponse.status === 418) {
+          setErrorMsg(` 418 I'm a teapot`);
+        }
+      }
+    } catch (error) {
+      console.log("API error => ", error);
+    };
 
-  return (
+    return (
 
-    <div >
-      <h1>Starwars Character</h1>
-      <h2>Name : {character}</h2>
-    </div>
-  );
-}
+      <div >
+        <h1>Starwars Character</h1>
+        <h2>Name : {character}</h2>
+        <h3>{errorMsg}</h3>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
